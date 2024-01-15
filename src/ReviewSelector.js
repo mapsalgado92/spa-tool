6c56faee20e43fced5002d430434d2eee149bfe6
Mario Salgado <mapsalgado92@gmail.com>
2024-01-15 00:01:02 +0000

    adding updated user problem secondary tag

--- src/ReviewSelector.js
+++ src/ReviewSelector.js
@@ -108,11 +108,15 @@ const ReviewSelector = ({ data, filter, selected, select_handler, peek }) => {
                   ))}
                 {(record.user_problem === "Undefined user problem" ||
                   !record.user_problem) &&
-                  !record.updated_user_problem && (
-                    <span className="tag is-warning is-light is-rounded is-small">
+                  (record.updated_user_problem ? (
+                    <span className="tag is-warning is-rounded is-small">
                       u
                     </span>
-                  )}
+                  ) : (
+                    <span className="tag is-secondary is-light is-rounded is-small">
+                      u
+                    </span>
+                  ))}
               </span>
 
               <span className="ml-1">
